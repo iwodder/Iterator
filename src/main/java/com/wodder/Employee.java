0ca26ab225658ca47b520dc846e66030153bce5d
Ian Wodder <ian.wodder@gmail.com>
2020-09-25 20:07:24 -0500

    Remove usage of preview features

--- src/main/java/com/wodder/Employee.java
+++ src/main/java/com/wodder/Employee.java
@@ -1,9 +1,31 @@
 package com.wodder;
 
-record Employee(String fName, String lName, Long salary) {
+public class Employee {
+
+    private final String fName;
+    private final String lName;
+    private final long salary;
+
+    public Employee(String fName, String lName, Long salary) {
+        this.fName = fName;
+        this.lName = lName;
+        this.salary = salary;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public long getSalary() {
+        return salary;
+    }
 
     @Override
     public String toString() {
-        return "My name is " + fName() + " " + lName();
+        return "My name is " + getfName() + " " + getlName();
     }
 }
